118ba53e725b17b9aceea457fc3c73d2cdac7f69
Ted Yeates <t3d.y34t3s@gmail.com>
2017-02-25 19:26:57 +0000

    added separators for ends of different components

--- src/RunwaySideView.java
+++ src/RunwaySideView.java
@@ -33,6 +33,7 @@ public class RunwaySideView extends JPanel{
     }
 
     //for testing only
+    //----------------------
     public static void main(String[] args) {
         JFrame jframe = new JFrame();
         jframe.setSize(1000, 400);
@@ -44,14 +45,25 @@ public class RunwaySideView extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        this.drawRunway(g, runwayLength);
-
+        this.drawRunway(g);
+        this.drawSeparatorEnd(g, runways.get("TODA"));
+        this.drawSeparatorEnd(g, runways.get("TORA"));
+        this.drawSeparatorEnd(g, runways.get("ASDA"));
+        this.drawSeparatorEnd(g, runways.get("LDA"));
     }
+    //--------------------------
 
     //draws runway
-    private void drawRunway(Graphics g, int length){
+    private void drawRunway(Graphics g){
         g.setColor(Color.black);
-        g.fillRect(start, Y, length, HEIGHT);
+        g.fillRect(start, Y, runwayLength, HEIGHT);
+    }
+
+    //draws a seperator to see ends of different strip components
+    private void drawSeparatorEnd(Graphics g, int length){
+        //x is where the runway component end
+        //height is altered so separator is visible
+        g.fillRect(start + length, Y, 2, HEIGHT + 10);
     }
 
 
