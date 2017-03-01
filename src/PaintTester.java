2dfcaffe855f294ff659ab49e3e5f5ce02c1da1c
Ted Yeates <t3d.y34t3s@gmail.com>
2017-03-01 08:09:15 +0000

    Simple Gui (doesnt do anything yet)

--- src/PaintTester.java
+++ src/PaintTester.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PaintTester extends JPanel {
 
@@ -7,7 +8,27 @@ public class PaintTester extends JPanel {
    private ObstacleBack obs;
    private ObstacleView obsView;
 
+    private int LDAStart;
+    private int start;
+    private int TODALength;
+    private int TORALength;
+    private int ASDALength;
+    private int LDALength;
+    private int runwayLength;
+    private int runwayNumber;
+    private int runwayHeight;
+    private int offsetX;
+    private int offsetY;
+    private int offsetZ;
+
+    ArrayList<JLabel> jLabels;
+    ArrayList<JTextField> jTextAreas;
+
     public PaintTester() {
+
+        jLabels = new ArrayList<>();
+        jTextAreas = new ArrayList<>();
+
         int[] x = {0, 10, 30, 10, 0};
         int[] y = {0, 0, 10, 20, 20};
 
@@ -30,6 +51,8 @@ public class PaintTester extends JPanel {
         JFrame jframe = new JFrame();
         jframe.setSize(1000, 400);
 
+        pt.GUI();
+
         jframe.add(pt);
         jframe.setVisible(true);
     }
@@ -45,5 +68,34 @@ public class PaintTester extends JPanel {
 
     public RunwayView getRsw() {
         return rsw;
+    }
+
+    public void GUI(){
+        JFrame jFrame = new JFrame();
+        jLabels.add(new JLabel("LDAStart"));
+        jLabels.add(new JLabel("Start"));
+        jLabels.add(new JLabel("TODA Length"));
+        jLabels.add(new JLabel("TORA Length"));
+        jLabels.add(new JLabel("ASDA Length"));
+        jLabels.add(new JLabel("LDA Length"));
+        jLabels.add(new JLabel("Runway Length"));
+        jLabels.add(new JLabel("Runway Number"));
+        jLabels.add(new JLabel("Runway Height"));
+        jLabels.add(new JLabel("Offset X"));
+        jLabels.add(new JLabel("Offset Y"));
+        jLabels.add(new JLabel("Offset Z"));
+
+        JButton jButton = new JButton("OK");
+
+        for (int i = 0; i < jLabels.size(); i++){
+            jFrame.add(jLabels.get(i));
+
+            jFrame.add(new TextField(5));
+            jFrame.add(jButton);
+        }
+
+        jFrame.setSize(1000, 400);
+        jFrame.setLayout(new FlowLayout());
+        jFrame.setVisible(true);
     }
 }
